d1775c5242a31bc64a2187663aefca1a5c7afdea
mendi kuperman <mendikuperman@gmail.com>
2025-09-08 17:53:50 +0300

    feat:add option to send to elastic

---
+++ dal/elastic_dal.py
@@ -0,0 +1,24 @@
+from shared.connectors.elastic_connector import Elastic_Connector
+from shared.utils.logger import logger
+import json
+
+class Elastic_DAL:
+
+    def __init__(self):
+        self.es = Elastic_Connector().es
+
+    def index_doc(self ,index_name,doc_id,doc):
+        self.es.index(index=index_name,id= doc_id, document=doc)
+        logger.info(f"Indexed to ES: {doc_id}")
+
+    def get_all_data(self,index_name):
+        """Retrieve all documents from the index."""
+        logger.info(f"Retrieving all documents from {index_name} index")
+
+        res = self.es.search(index=index_name, query={"match_all": {}}, size=10000)
+        docs = [hit["_source"] for hit in res["hits"]["hits"]]
+
+        logger.info(f"Retrieved {len(docs)} documents")
+
+        print(json.dumps(docs ,indent= 4))
+
---
+++ services/es_indexer_service/src/main.py
@@ -0,0 +1,4 @@
+from services.es_indexer_service.src.consumer import ESIndexer
+
+if __name__ == "__main__":
+    ESIndexer().run()
